8ad19436c5d10780334901983e42584de57cfda2
FenPhoenix <FenPhoenix@users.noreply.github.com>
2025-04-02 13:40:18 -0700

    Change paths

--- DarkSaveManager/Paths.cs
+++ DarkSaveManager/Paths.cs
@@ -2,10 +2,12 @@
 
 internal static class Paths
 {
-    internal static readonly string SaveStore = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "DarkSaveManager");
+    internal static readonly string Data = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "DarkSaveManager");
+
+    internal static readonly string SaveStore = Path.Combine(Data, "SaveStore");
 
     // TODO: Make this a random folder name to avoid naming conflicts
     internal static readonly string Temp = Path.Combine(Path.GetTempPath(), "DarkSaveManager");
 
-    internal static readonly string ConfigIni = Path.Combine(AppContext.BaseDirectory, "Config.ini");
+    internal static readonly string ConfigIni = Path.Combine(Data, "Config.ini");
 }
